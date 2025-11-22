77169dade272665d80c6ed3f85d4e333bbbb51c0
sumon10c <sumonchakrobarty9466@gmail.com>
2025-11-23 00:06:24 +0600

    add all crops loading section

--- src/component/AllCrops/Allcrops.jsx
+++ src/component/AllCrops/Allcrops.jsx
@@ -3,14 +3,29 @@ import CropCard from "./CropCard";
 
 const Allcrops = () => {
   const [crops, setCrops] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:3000/products")
       .then((res) => res.json())
-      .then((data) => setCrops(data))
-
-      .catch((error) => console.log(error));
+      .then((data) => {
+        setCrops(data);
+        setLoading(false); 
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false); 
+      });
   }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-[50vh]">
+        <span className="loading loading-spinner loading-lg"></span>
+        
+      </div>
+    );
+  }
 
   return (
     <div>
